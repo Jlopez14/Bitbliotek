09ad8ba0b1117b15f65138c46d5e58859c0244d0
Jlopez14 <lopez.lujos22@cordoba.salesianos.edu>
2024-02-06 18:02:29 +0100

    Subidote 06.02.2024

--- .idea/Interfaz/Ventana1.py
+++ .idea/Interfaz/Ventana1.py
@@ -9,42 +9,85 @@ from kivy.uix.label import Label
 from kivy.uix.popup import Popup
 from kivy.core.window import Window
 from conexion import obtener_contrasena
+from conexion import obtenerID
+from kivy.uix.widget import Widget
+from conexion import registrar
+
 class RegistrationWindow(BoxLayout):
+
+    def mostrar_popup(self, mensaje):
+        popup = Popup(title='Error de Autenticación',
+                      content=Label(text=mensaje),
+                      size_hint=(None, None), size=(400, 200))
+        popup.open()
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.orientation = 'vertical'
-        self.padding = dp(20)
+        self.padding = (dp(20), dp(2))
+
+
+        # Mueve la ventana emergente un poco más arriba
+        self.pos_hint = {'center_x': 0.5, 'center_y': 0.5}
 
         # Agregar etiquetas y campos de registro
-        self.add_widget(Label(text='Registro', size_hint=(None, None), height=dp(30)))
-
-        self.add_widget(Label(text='Nombre de usuario:', size_hint=(None, None), height=dp(30)))
-        self.username_input = TextInput(hint_text='Ingrese su nombre de usuario', multiline=False, size_hint=(None, None), height=dp(40))
+        self.add_widget(Label(text='Nombre de usuario:', size_hint=(None, None), height=dp(36), pos_hint={'center_x': 0.5}, font_size=14, bold=True))
+        self.username_input = TextInput(hint_text='Ingrese su nombre de usuario', multiline=False, size_hint=(1, None), height=dp(40), pos_hint={'center_x': 0.5})
         self.add_widget(self.username_input)
 
+        self.add_widget(Label(text='Apellidos de usuario:', size_hint=(None, None), height=dp(36), pos_hint={'center_x': 0.5}, font_size=14, bold=True))
+        self.lastname_input = TextInput(hint_text='Ingrese su apellido de usuario', multiline=False, size_hint=(1, None), height=dp(40), pos_hint={'center_x': 0.5})
+        self.add_widget(self.lastname_input)
 
-        self.add_widget(Label(text='Correo electrónico:', size_hint=(None, None), height=dp(30)))
-        self.email_input = TextInput(hint_text='Ingrese su correo electrónico', multiline=False, size_hint=(None, None), height=dp(40))
+        self.add_widget(Label(text='Correo electrónico:', size_hint=(None, None), height=dp(36), pos_hint={'center_x': 0.5}, font_size=14, bold=True))
+        self.email_input = TextInput(hint_text='Ingrese su correo electrónico', multiline=False, size_hint=(1, None), height=dp(40), pos_hint={'center_x': 0.5})
         self.add_widget(self.email_input)
 
-        self.add_widget(Label(text='Contraseña:', size_hint=(None, None), height=dp(30)))
-        self.password_input = TextInput(hint_text='Ingrese su contraseña', multiline=False, password=True, size_hint=(None, None), height=dp(40))
+        self.add_widget(Label(text='Contraseña:', size_hint=(None, None), height=dp(36), pos_hint={'center_x': 0.5}, font_size=14, bold=True))
+        self.password_input = TextInput(hint_text='Ingrese su contraseña', multiline=False, password=True, size_hint=(1, None), height=dp(40), pos_hint={'center_x': 0.5})
         self.add_widget(self.password_input)
 
-        self.add_widget(Button(text='Registrar', on_press=self.on_register_press, size_hint=(None, None), height=dp(40)))
+        self.add_widget(Label(text='Repite Contraseña:', size_hint=(None, None), height=dp(36), pos_hint={'center_x': 0.5}, font_size=14, bold=True))
+        self.password2_input = TextInput(hint_text='Repita su contraseña', multiline=False, password=True, size_hint=(1, None), height=dp(40), pos_hint={'center_x': 0.5})
+        self.add_widget(self.password2_input)
+
+        # Agregar un espacio vertical
+        self.add_widget(Widget(size_hint_y=None, height=dp(20)))
+
+        # Agregar botones en la misma línea horizontal
+        buttons_layout = BoxLayout(size_hint=(1, None), height=dp(40), spacing=236, pos_hint={'center_x': 0.5})
+        buttons_layout.add_widget(Button(text='Registrar', on_press=self.on_register_press, size_hint=(None, None), height=dp(40)))
+
+        # Agregar el botón "Cerrar" a la derecha y un poco más a la derecha
+        buttons_layout.add_widget(Button(text='Cerrar', on_press=self.on_close_press, size_hint=(None, None), height=dp(40), pos_hint={'right': 1}))
+        self.add_widget(buttons_layout)
 
     def on_register_press(self, instance):
         # Acción a realizar cuando se presiona el botón "Registrar" en la ventana de registro
         username = self.username_input.text
+        lastname = self.lastname_input.text
         email = self.email_input.text
         password = self.password_input.text
+        repassword = self.password2_input.text
 
-        # Puedes realizar alguna acción con los datos, como imprimirlos en la consola
-        print(f'Nuevo Usuario: {username}, Correo electrónico: {email}, Nueva Contraseña: {password}')
+        if password != repassword:
+            self.mostrar_popup("Las contraseñas no coinciden")
 
-        # Aquí podrías agregar lógica adicional, como almacenar la información en una base de datos, etc.
+        elif username == '' or lastname == '' or email == '' or password == '' or repassword == '':
+            self.mostrar_popup("Por favor complete todos los campos")
 
-class MyApp(App):
+        else:
+            registrar(username,lastname,email,password)
+            self.mostrar_popup("Usuario registrado correctamente")
+            App.get_running_app().stop()
+            Login().run()
+
+    def on_close_press(self, instance):
+        App.get_running_app().stop()
+        Login().run()
+
+#Clase principal donde está la ventana 1
+class Login(App):
     def mostrar_popup(self, mensaje):
         popup = Popup(title='Error de Autenticación',
                       content=Label(text=mensaje),
@@ -108,7 +151,7 @@ class MyApp(App):
         # Acción a realizar cuando se presiona el botón "Registrar"
         # Crear y mostrar la ventana de registro
         registration_window = RegistrationWindow()
-        popup = Popup(title='Registro', content=registration_window, size_hint=(None, None), size=(dp(300), dp(400)))
+        popup = Popup(title='Registro de usuario', content=registration_window, size_hint=(None, None), size=(dp(500), dp(500),))
         popup.open()
 
     def on_button_press(self, instance):
@@ -117,13 +160,20 @@ class MyApp(App):
         password_text = instance.parent.children[2].text  # Obtener el texto del campo de contraseña
 
         contrasenna=obtener_contrasena(user_text)
+        id=obtenerID(user_text) #Obtiene el ID
+
+
+        App.get_running_app().stop #Cierra la ventana actual
+        Inicio(id).run() #Abre la nueva ventana mandando el ID
 
         if password_text!=contrasenna:
-            self.mostrar_popup("Contraseña incorrecta")
+            self.mostrar_popup("Usuario y/o contraseña incorrecta")
+
 
     def on_exit_press(self, instance):
-        # Acción a realizar cuando se presiona el botón "Salir"
-        App.get_running_app().stop()
+        # Acción a realizar cuando se presiona el botón "Cerrar" en la ventana emergente
+        if self.popup_instance:
+            self.popup_instance.dismiss()
 
 if __name__ == '__main__':
-    MyApp().run()
+    Login().run()
